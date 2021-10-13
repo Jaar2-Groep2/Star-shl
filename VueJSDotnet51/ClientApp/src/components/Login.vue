33618d5494b413e12531451e29944029614ff1a4
Flatn3ck <74960799+Flatn3ck@users.noreply.github.com>
2021-10-14 00:54:13 +0200

    Geoefend met login pagina maken

---
+++ VueJSDotnet51/ClientApp/src/components/Login.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1 id="tableLabel">Login</h1>
+
+    <p>Hier kan je inloggen</p>
+
+</template>
+
+
+<script>
+</script>
--- VueJSDotnet51/ClientApp/src/components/NavMenu.vue
+++ VueJSDotnet51/ClientApp/src/components/NavMenu.vue
@@ -2,7 +2,7 @@
     <header>
         <nav class="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3">
             <div class="container">
-                <a class="navbar-brand">Vue JS Template for .NET 5</a>
+                <a class="navbar-brand">Star-shl</a>
                 <button class="navbar-toggler"
                         type="button"
                         data-toggle="collapse"
@@ -19,10 +19,7 @@
                            
                         </li>
                         <li class="nav-item">
-                            <router-link :to="{ name: 'Counter' }" class="nav-link text-dark">Counter</router-link>
-                        </li>
-                        <li class="nav-item">
-                            <router-link :to="{ name: 'FetchData' }" class="nav-link text-dark">Fetch Data</router-link>
+                            <router-link :to="{ name: 'Login' }" class="nav-link text-dark">Login</router-link>
                         </li>
                     </ul>
                 </div>
