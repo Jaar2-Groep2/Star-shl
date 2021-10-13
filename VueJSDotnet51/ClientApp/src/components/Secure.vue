83f7f89f0e720e27cb06cfb5cd353942bacaad17
Flatn3ck <74960799+Flatn3ck@users.noreply.github.com>
2021-10-14 01:13:57 +0200

    login scherm

---
+++ VueJSDotnet51/ClientApp/src/components/Secure.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="Secure">
+        <h1>Secure Area</h1>
+        <p>
+            This is a Secure area
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Secure',
+        data() {
+            return {};
+        }
+    }
+</script>
+
+<style scoped>
+    #secure {
+        background-color: #FFFFFF;
+        border: 1px solid #CCCCCC;
+        padding: 20px;
+        margin-top: 10px;
+    }
+</style>
