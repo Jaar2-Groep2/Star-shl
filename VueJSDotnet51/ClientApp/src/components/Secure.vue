6ca822fe167c4a0b45cdeaba0651c04d2072d9a6
Esat-Aydin <esat61_2001@hotmail.com>
2021-11-15 00:58:55 +0100

    edit page button

--- VueJSDotnet51/ClientApp/src/components/Secure.vue
+++ VueJSDotnet51/ClientApp/src/components/Secure.vue
@@ -1,45 +1,61 @@
 <template>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
     <div id="Secure">
         <img src="../assets/star-shl.jpg" />
         <h1>Medewerkersportaal</h1>
-        <p>
-            Welkom Admin!
-        </p>
-    </div>
 
+    </div>
     <div class="Reservations">
         <a class="add waves-effect waves-light btn pink lighten-2 white-text hoverable">
-            <i class="material-icons">create</i> Reservering toevoegen
+            <i class="material-icons">add_circle</i> Reservering toevoegen
         </a>
 
         <a class="remove waves-effect waves-light btn pink lighten-2 white-text hoverable">
-            <i class="material-icons">create</i> Reservering verwijderen
+            <i class="material-icons">delete</i> Reservering verwijderen
         </a>
 
         <a class="see waves-effect waves-light btn pink lighten-2 white-text hoverable">
-            <i class="material-icons">create</i> Reserveringen inzien
+            <i class="material-icons">visibility</i> Reserveringen inzien
         </a>
     </div>
 
-    <div class="Edit_page">
-        <a class="waves-effect waves-light btn pink lighten-2 white-text hoverable">
-            pagina bewerken<i class="material-icons">create</i>
-        </a>
+    <!-- Dropdown Trigger -->
+
+    <a class='dropdown-trigger btn' href='#' data-target='dropdown1'><j class="material-icons">create</j>pagina bewerken</a>
+
+    <!-- Dropdown Structure with materialize: does not work-->
+    <ul id='dropdown1' class='dropdown-content'>
+        <li><a href="#">Home-pagina bewerken</a></li>
+        <li><a href="#">Informatie-pagina bewerken</a></li>
+    </ul>
+
+    <div class="dropdown"> <!-- dropdown structure with bootstrap: does not work-->
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Dropdown button
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+        </div>
     </div>
+
 </template>
 
 
 <script>
-    export default {
-        name: 'Secure',
-        emits: ["authenticated", "setAuthenticated"],
-        data() {
-            return {};
-        }
-    }
+
+    document.addEventListener('DOMContentLoaded', function () {
+        var elems = document.querySelectorAll('.dropdown-trigger');
+        var options = {alignment: 'left'}
+        this.Dropdown.init(elems, options);
+    });
+
+
 </script>
 
 <style scoped>
@@ -48,19 +64,29 @@
 
     .Reservations a {
         font-size: 15px;
-        margin: 3%; 
+        margin: 1%; 
         width: 20%;
         border-radius: 15px;
     }
 
     .Reservations a i {
-        font-size: 15px;
+        font-size: 16px;
+    }
+    
+    .a j {
+        font-size: 20px;
     }
 
-    .Edit_page{
+    .Edit_page_Button a{
         font-size: 15px;
-        margin-top: 5%;
-        width: 15%;
-        border-radius: 7px;
+        margin: 3%;
+        width: 20%;
+        border-radius: 15px;
+    }
+    .Editable_pages a {
+        font-size: 15px;
+        margin: 3%;
+        width: 20%;
+        border-radius: 15px;
     }
 </style>
