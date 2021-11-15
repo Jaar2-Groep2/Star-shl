df856d87ef829323c81d24866d66441731dc52cc
0992392 <80328928+NuurSaid@users.noreply.github.com>
2021-11-15 20:20:16 +0100

    buttons without framework

--- VueJSDotnet51/ClientApp/src/components/Secure.vue
+++ VueJSDotnet51/ClientApp/src/components/Secure.vue
@@ -2,38 +2,33 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!--Import materialize.css-->
     <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
-
-    <!-- DEZE FUCKT UP MET DROPDOWN MENU
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-
-    -->
+    
     <div class="headline">
         <h2 style="font-size:7vw">Medewerkersportaal</h2>
     </div>
     <div id="Secure">
-        <img src="../assets/star-shl.jpg" />
+        <img src="../assets/star-shl.jpg" /> 
 
     </div>
     <div class="Reservations">
-        <a class="add waves-effect waves-light btn pink lighten-2 white-text hoverable">
+        <a class="btn">
             <i class="material-icons">add_circle</i> Reservering toevoegen
         </a>
 
-        <a class="remove waves-effect waves-light btn pink lighten-2 white-text hoverable">
+        <a class="btn">
             <i class="material-icons">delete</i> Reservering verwijderen
         </a>
 
-        <a class="see waves-effect waves-light btn pink lighten-2 white-text hoverable">
+        <a class="btn">
             <i class="material-icons">visibility</i> Reserveringen inzien
         </a>
     </div>
 
-    <div class="Editable_pages">
-        <a class="see waves-effect waves-light btn green lighten-2 white-text hoverable">
-            <p>Informatiepagina bewerken</p>
+    <div class="Editable_pages Reservations">
+        <a class="btn">
+            Informatiepagina bewerken
         </a>
-        <a class="see waves-effect waves-light btn green lighten-2 white-text hoverable">
+        <a class="btn">
             Homepagina bewerken
         </a>
     </div>
@@ -60,34 +55,23 @@
         vertical-align: middle;
         border-style: none;
         width: 60%;
-    }
-
-    .Reservations a {
-        font-size: 70%;
-        margin: 3%; 
-        width: 60%;
-        border-radius: 15px;
+        margin-bottom: 15%
     }
 
     .Reservations a i {
         font-size: 16px;
     }
-    
-    .a j {
-        font-size: 20px;
+
+    .Reservations a {
+        background: #e91e63;
+        border: hidden;
+        font-size: 50%;
+        border-radius: 15px;
+        width: 60%;
+        margin: 3%;
     }
 
-    .Edit_page_Button a {
-        font-size: 7vw;
-        margin: 3%;
-        width: 10%;
-        border-radius: 15px;
-    }
     .Editable_pages a {
-        font-size: 70%;
-        margin: 3%;
-        width:60%;
-        border-radius: 15px;
+        background: #00c853;
     }
-
 </style>
