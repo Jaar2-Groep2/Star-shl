2f64c17317c52ee812b3aecc80285483d07cd4b3
0992392 <80328928+NuurSaid@users.noreply.github.com>
2021-11-16 16:15:32 +0100

    textboxes

--- VueJSDotnet51/ClientApp/src/components/Home.vue
+++ VueJSDotnet51/ClientApp/src/components/Home.vue
@@ -3,27 +3,23 @@
         <div class="img">
             <img src="../assets/star-shl.jpg" />
         </div>
+
         <video controls="controls" preload="metadata">
             <source src="../assets/WatGebeurtErMetBloed.mp4#t=3.5" type="video/mp4">
             Your browser does not support the video tag.
         </video>
+
         <div class="text_homepage">
-            
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus maximus nisi vel dolor fermentum euismod. Ut dictum pellentesque sem, nec tristique tellus facilisis eu. Quisque commodo sem ipsum, vitae varius libero sollicitudin quis. In rhoncus feugiat ante, non viverra quam. Vivamus efficitur ligula ut odio sollicitudin posuere. Nam id tincidunt nisi. Donec eleifend ipsum ut odio porttitor, nec tempus lorem egestas. Curabitur pretium hendrerit mauris. Maecenas bibendum risus eros, eu pharetra sapien convallis et. Quisque sed purus sollicitudin, ultricies metus ut, pulvinar risus. Sed non sapien malesuada ligula dictum venenatis eget sit amet dui. Cras vitae arcu id tellus vestibulum ultricies. Praesent iaculis nulla vel dui tristique mollis. Quisque accumsan eu odio eget rutrum.
-                   
+
+            <p>
+                Iedereen die bij ons komt, heeft vragen over gezondheid. Vragen die wij beantwoorden met betrouwbare medische diagnostiek.
+                Achter elke diagnose schuilt een persoonlijk verhaal. <strong class="black-text">Klik om meer te lezen.</strong>
+            </p>
+
+            <p>*Informatie over bloedprikken* <br><strong class="black-text">Klik om meer te lezen.</strong></p>
+
+            <p>*Priklocaties* <br><strong class="black-text">Klik om meer te lezen.</strong></p>
         </div>
-    </div>
-
-    <div class="AdminLoginButton">
-        <a class="grey-text" v-on:click="login()">
-            Inloggen voor medewerkers
-        </a>
-    </div>
-
-    <div class="Language">
-        <a class="grey-text">
-            <i class="material-icons">language</i>Land/Taal
-        </a>
     </div>
 </template>
 
@@ -45,28 +41,27 @@
         width: 70%
     }
 
-    .AdminLoginButton a {
-        font-size: 10px;
-        cursor: pointer;
-    }
-
-    .AdminLoginButton {
-        margin-top: 6%;
-        margin-bottom: 2%;
-    }
-
-    .Language {
-        cursor: pointer;
-    }
-
-    .Language i {
-            font-size: 15px;
-    }
-
-    .text_homepage{
+    .text_homepage {
+        font-size: 70%;
         text-align: center;
-        margin-left:15%;
-        margin-right:15%;
+        margin-top: 5%;
+        margin-left: 8%;
+        margin-right: 8%;
+    }
+
+    .homeButtons {
+        margin-top: 4%;
+        font-size: 70%;
+    }
+
+    .text_homepage p {
+        cursor: pointer;
+        padding: 1%;
+        margin-top: 5%;
+        margin-right: 10%;
+        margin-left: 10%;
+        border-radius: 15px;
+        box-shadow: 0 0 10px 1px black;
     }
 
     video {
