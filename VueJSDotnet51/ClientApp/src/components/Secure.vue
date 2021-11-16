6b7492056d2a51330b80f813d720e2f6d13789b9
Esat-Aydin <esat61_2001@hotmail.com>
2021-11-16 11:44:48 +0100

    Medewerkersportaal is voor nu af qua knoppen

--- VueJSDotnet51/ClientApp/src/components/Secure.vue
+++ VueJSDotnet51/ClientApp/src/components/Secure.vue
@@ -2,36 +2,44 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!--Import materialize.css-->
     <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+
+    <div id="Secure">
+        <img src="../assets/star-shl.jpg" />
     
     <div class="headline">
-        <h2 style="font-size:7vw">Medewerkersportaal</h2>
+        <h2 style="font-size:6vw">Medewerkersportaal</h2>
     </div>
-    <div id="Secure">
-        <img src="../assets/star-shl.jpg" /> 
-
+    
     </div>
-    <div class="Reservations">
+    <div class="Reservation_add Reservations">
         <a class="btn">
             <i class="material-icons">add_circle</i> Reservering toevoegen
         </a>
-
+    </div>
+    <div class="Reservation_delete Reservations">
         <a class="btn">
             <i class="material-icons">delete</i> Reservering verwijderen
         </a>
+    </div>
 
+    <div class="reservation_check Reservations">
         <a class="btn">
             <i class="material-icons">visibility</i> Reserveringen inzien
         </a>
     </div>
 
-    <div class="Editable_pages Reservations">
-        <a class="btn">
-            Informatiepagina bewerken
-        </a>
-        <a class="btn">
-            Homepagina bewerken
-        </a>
-    </div>
+
+        <div class="Editable_infopage Reservations">
+            <a class="btn">
+                <i class="material-icons">edit</i>Informatiepagina bewerken
+            </a>
+        </div>
+
+        <div class="Editable_homepage Reservations">
+            <a class="btn">
+                <i class="material-icons">edit</i>Homepagina bewerken
+            </a>
+        </div>
 
 
 </template>
@@ -55,7 +63,7 @@
         vertical-align: middle;
         border-style: none;
         width: 60%;
-        margin-bottom: 15%
+        margin-bottom: 3%
     }
 
     .Reservations a i {
@@ -68,10 +76,22 @@
         font-size: 50%;
         border-radius: 15px;
         width: 60%;
-        margin: 3%;
+        margin: 2%;
+    }
+    .Reservation_add{
+        opacity: 0.2;
+    }
+    .Reservation_delete{
+        opacity: 0.2;
+    }
+    .Editable_homepage a {
+    
+        opacity: 0.2;
+        background: #00c853;
     }
 
-    .Editable_pages a {
+    .Editable_infopage a {
+        opacity: 0.2;
         background: #00c853;
     }
 </style>
