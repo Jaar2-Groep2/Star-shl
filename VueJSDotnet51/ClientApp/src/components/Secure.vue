62203b373e82a1869a6a7cc9f5267a3b9b905fb8
Esat-Aydin <esat61_2001@hotmail.com>
2021-11-16 12:06:31 +0100

    alle stukjes code voorzien van informatie

--- VueJSDotnet51/ClientApp/src/components/Secure.vue
+++ VueJSDotnet51/ClientApp/src/components/Secure.vue
@@ -3,39 +3,40 @@
     <!--Import materialize.css-->
     <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
 
-    <div id="Secure">
+    <div id="Secure">   <!-- star shl banner-->
         <img src="../assets/star-shl.jpg" />
+    </div>
     
-    <div class="headline">
+    <div class="headline">  <!-- Headline text-->
         <h2 style="font-size:6vw">Medewerkersportaal</h2>
     </div>
     
-    </div>
-    <div class="Reservation_add Reservations">
+    <div class="Reservation_add Reservations">  <!-- Reservation add-->
         <a class="btn">
             <i class="material-icons">add_circle</i> Reservering toevoegen
         </a>
     </div>
-    <div class="Reservation_delete Reservations">
+
+    <div class="Reservation_delete Reservations">   <!-- Reservation delete-->
         <a class="btn">
             <i class="material-icons">delete</i> Reservering verwijderen
         </a>
     </div>
 
-    <div class="reservation_check Reservations">
+    <div class="reservation_check Reservations">    <!-- Reservation check-->
         <a class="btn">
             <i class="material-icons">visibility</i> Reserveringen inzien
         </a>
     </div>
 
 
-        <div class="Editable_infopage Reservations">
+        <div class="Editable_infopage Reservations">    <!-- Information page edit button -->
             <a class="btn">
                 <i class="material-icons">edit</i>Informatiepagina bewerken
             </a>
         </div>
 
-        <div class="Editable_homepage Reservations">
+        <div class="Editable_homepage Reservations">    <!-- Home page edit button -->
             <a class="btn">
                 <i class="material-icons">edit</i>Homepagina bewerken
             </a>
@@ -46,7 +47,7 @@
 
 
 <script>
-
+    //materialise code voor dropdown button niet toegepast
     document.addEventListener('DOMContentLoaded', function () {
         var elems = document.querySelectorAll('.dropdown-trigger');
         var options = {alignment: 'left'}
@@ -73,10 +74,11 @@
     .Reservations a {
         background: #e91e63;
         border: hidden;
+        padding: 15px;
         font-size: 50%;
         border-radius: 15px;
         width: 60%;
-        margin: 2%;
+        margin: 1.5%;
     }
     .Reservation_add{
         opacity: 0.2;
