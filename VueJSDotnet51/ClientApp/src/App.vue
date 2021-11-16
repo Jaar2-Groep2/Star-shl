eade85029de428db4e793d06205892ba5184c2ea
Flatn3ck <74960799+Flatn3ck@users.noreply.github.com>
2021-11-16 11:51:56 +0100

    Update App.vue

--- VueJSDotnet51/ClientApp/src/App.vue
+++ VueJSDotnet51/ClientApp/src/App.vue
@@ -69,7 +69,7 @@
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
         color: #2c3e50;
-        margin-top: 60px;
+        
         background-color: #e0edf5;
     }
 </style>
