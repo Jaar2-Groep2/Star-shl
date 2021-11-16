b75570c28bbc3fc17a9f04552c3b34a6c6332796
Kristian de Lange <dubbelkris18@outlook.com>
2021-11-16 11:17:11 +0100

    Example sql class, next up: docker setup (again)

--- VueJSDotnet51/Controllers/DatabaseController.cs
+++ VueJSDotnet51/Controllers/DatabaseController.cs
@@ -22,33 +22,13 @@ namespace VueJSDotnet51.Controllers
         {
             _logger = logger;
         }
-        //kinda working, only username returned, error with the function though, not with the sql connection
+        //Please place the used query in commentary here and call in Database_Handling.Database
+        //Like this:
+        //"select username, h_pword from Admins"
         [HttpGet]
         public IEnumerable<Sqltest> Get()
         {
-            String connectionString = $"Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename={Directory.GetCurrentDirectory() }\\App_Data\\Database1.mdf;Integrated Security=True";
-
-            String queryResult = "select username, h_pword from Admins";
-            var connection = new SqlConnection(connectionString);
-            connection.Open();
-
-            SqlCommand command = new SqlCommand(queryResult, connection);
-
-            // Do work here; connection closed on following line.
-            using SqlDataReader reader = command.ExecuteReader();
-            List<Sqltest> returner = new List<Sqltest>();
-            while (reader.Read())
-            {
-                //Console.WriteLine(String.Format("{0}, {1}",
-                //reader[0], reader[1]));
-
-                Sqltest value = new Sqltest(reader.GetString(0), reader.GetString(1));
-                //Use this as break point, value should have the requested data
-                Console.WriteLine();
-                returner.Add(value);
-            }
-            connection.Close();
-            return returner;
+            return Database_Handling.Database.ExampleQuery();
         }
     }
 }
---
+++ VueJSDotnet51/Database_Handling/Database.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VueJSDotnet51.Database_Handling
+{
+    /// <summary>Static Class Database handles all queries</summary>
+    public static class Database
+    {
+        //Create connection
+        private static SqlConnection P_CreateConnection() => new SqlConnection($"Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename={Directory.GetCurrentDirectory() }\\App_Data\\Database1.mdf;Integrated Security=True");
+        //Actual query excecuter per Type, can be used with multiple queries
+        private static List<Sqltest> P_ExampleQuery(string query) 
+        {
+            SqlConnection sqlConnection = P_CreateConnection();
+            SqlCommand command = new SqlCommand(query, sqlConnection);
+            sqlConnection.Open();
+
+            using SqlDataReader reader = command.ExecuteReader();
+            List<Sqltest> returner = new List<Sqltest>();
+            while (reader.Read())
+            {
+                Sqltest value = new Sqltest(reader.GetString(0), reader.GetString(1));
+                //Use this as break point, value should have the requested data
+                returner.Add(value);
+            }
+
+            sqlConnection.Close();
+            return returner;
+        }
+        //Excecuted query, for clarity I expect all Sql statements in this class
+        public static List<Sqltest> ExampleQuery() => P_ExampleQuery("select username, h_pword from Admins");
+    }
+}
