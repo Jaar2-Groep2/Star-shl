5541004cdf2a42d5782f75fdc820bf90b5afddd3
0992392 <80328928+NuurSaid@users.noreply.github.com>
2021-11-16 21:05:43 +0100

    Added buttons and extra information to HomeScreen

--- VueJSDotnet51/ClientApp/src/components/Home.vue
+++ VueJSDotnet51/ClientApp/src/components/Home.vue
@@ -10,16 +10,32 @@
         </video>
 
         <div class="text_homepage">
-
+            <h6>Over Star-shl</h6>
             <p>
                 Iedereen die bij ons komt, heeft vragen over gezondheid. Vragen die wij beantwoorden met betrouwbare medische diagnostiek.
-                Achter elke diagnose schuilt een persoonlijk verhaal. <strong class="black-text">Klik om meer te lezen.</strong>
+                Achter elke diagnose schuilt een persoonlijk verhaal.
+                Daarom geloven wij dat elke vraag onze speciale aandacht verdient voor een helder en betrouwbaar antwoord.<br>
+                <strong class="black-text">Klik om meer te lezen.</strong>
             </p>
-
-            <p>*Informatie over bloedprikken* <br><strong class="black-text">Klik om meer te lezen.</strong></p>
-
-            <p>*Priklocaties* <br><strong class="black-text">Klik om meer te lezen.</strong></p>
         </div>
+
+        <a class="homeButtons btn">Informatie over onderzoeken</a>
+        <a class="homeButtons btn">Afspraak maken</a>
+        <a class="homeButtons btn">Priklocaties</a>
+        <br><br>
+        <a class="homeButtons blue btn">Contact</a>
+    </div>
+
+    <div class="ExtraInfo">
+        <p class="text-white">
+            <strong>Etten-Leur</strong><br>
+            Bredaseweg 165, 4872 LA<br>
+            Postbus 228, 4870 AE<br>
+            <br>
+            <strong>Rotterdam</strong><br>
+            Vlambloem 21, 3068 JE<br>
+            Postbus 8661, 3009 AR<br>
+        </p>
     </div>
 </template>
 
@@ -42,24 +58,19 @@
     }
 
     .text_homepage {
-        font-size: 70%;
+        font-size: 60%;
         text-align: center;
         margin-top: 5%;
         margin-left: 8%;
         margin-right: 8%;
     }
 
-    .homeButtons {
-        margin-top: 4%;
-        font-size: 70%;
-    }
-
     .text_homepage p {
         cursor: pointer;
         padding: 1%;
-        margin-top: 5%;
         margin-right: 10%;
         margin-left: 10%;
+        margin-bottom: 12%;
         border-radius: 15px;
         box-shadow: 0 0 10px 1px black;
     }
@@ -77,6 +88,32 @@
             margin-top: 10px;
             height: 240px;
         }
+    }
+
+    .homeButtons {
+        background: #E91E63;
+        border: hidden;
+        padding: 2%;
+        font-size: 50%;
+        border-radius: 15px;
+        width: 60%;
+        margin: 1.5%;
+    }
+
+    .blue {
+        background: #2C2B64;
+    }
+
+    .ExtraInfo {
+        margin-top: 5%;
+        background: #2C2B64;
+        padding: 5%;
+        padding-left: 8%;
+    }
+
+    .ExtraInfo p {
+        text-align: left;
+        font-size: 50%;
     }
 
 </style>
