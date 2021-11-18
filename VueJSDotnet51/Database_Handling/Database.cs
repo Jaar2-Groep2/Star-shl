7d763c64fb79f3969bd2915af9efca6c9a7dc0a3
Kristian de Lange <dubbelkris18@outlook.com>
2021-11-18 10:24:03 +0100

    Optimalisation of the database connection (could be improved even more with more knowledge about the way program.cs functions)

--- VueJSDotnet51/Database_Handling/Database.cs
+++ VueJSDotnet51/Database_Handling/Database.cs
@@ -9,64 +9,100 @@ using System.Threading.Tasks;
 namespace VueJSDotnet51.Database_Handling
 {
     /// <summary>Static Class Database handles all queries</summary>
+    /// In case of errors, open powershell or command line and run:
+    /// sqllocaldb stop star-shl
+    /// sqllocaldb delete star-shl
+    /// Then try again, if issues persists then search help
     public static class Database
     {
-        //Create connection
-        private static SqlConnection P_CreateConnection() => new($"Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename={Directory.GetCurrentDirectory() }\\App_Data\\Database1.mdf;Integrated Security=True");
-        //Actual query excecuter per Type, can be used with multiple queries
-        /*
-        private static List<Sqltest> P_ExampleQuery(string query) 
+        //Original method for comparison
+        //@Depricated
+        private static List<Sqltest> P2_ExampleQuery(string query)
         {
-            //Test();
-            SqlConnection sqlConnection = P_CreateConnection();
-            SqlCommand command = new(query, sqlConnection);
-            sqlConnection.Open();
-
-            using SqlDataReader reader = command.ExecuteReader();
-            List<Sqltest> returner = new();
-            while (reader.Read())
-            {
-                Sqltest value = new(reader.GetString(0), reader.GetString(1));
-                //Use this as break point, value should have the requested data
-                returner.Add(value);
-            }
-
-            sqlConnection.Close();
-            
-            return returner;
-        }
-         */
-        private static List<Sqltest> P_ExampleQuery(string query)
-        {
+            //Creates an object with info about the localdb installations
             ISqlLocalDbApi localDB = new SqlLocalDbApi();
-            //var instance = localDB.GetOrCreateInstance("MSSQLLocalDB");
+            //Starts a localdb instance with name "Star-shl" with localdb version 15 (download provided on the drive, TODO: Add this to system requirements)
             var instance = localDB.CreateInstance("Star-shl","15.0");
+            //Allows managing of the instance
             ISqlLocalDbInstanceManager manager = instance.Manage();
             if (!instance.IsRunning)
             {
                 manager.Start();
             }
-            //var example = localDB.CreateInstance("MSSQLLocalDB","15.0");
+            //creates the needed connectionstring
             var ConnectionString = instance.GetConnectionString()+ $";AttachDbFilename={Directory.GetCurrentDirectory() }\\App_Data\\Database1.mdf;Integrated Security=True";
+            //Opens the connection
             using SqlConnection connection = new(ConnectionString);
             connection.Open();
+            //Creates the command we want
             SqlCommand command = new(query, connection);
+            //Executes the query, output is a stream we need to turn into the requested output though
             using SqlDataReader reader = command.ExecuteReader();
+            //Creates the list we will return
             List<Sqltest> returner = new();
+            //Reads thourgh the stream
             while (reader.Read())
             {
+                //Gets the outpu per row
                 Sqltest value = new(reader.GetInt32(0), reader.GetString(1),reader.GetString(2), reader.GetString(3), reader.GetString(4), reader.GetString(5), reader.GetString(6));
                 //Use this as break point, value should have the requested data
                 returner.Add(value);
             }
-            Console.WriteLine();
+            //Console.WriteLine();
+            //CLoses the connection
             connection.Close();
             //"Data Source=np:\\\\.\\pipe\\LOCALDB#E0789834\\tsql\\query"
             //$"Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename={Directory.GetCurrentDirectory() }\\App_Data\\Database1.mdf;Integrated Security=True");
-            Console.WriteLine();
+            //Console.WriteLine();
+            //Stops the instance (at least it's supposed to)
             manager.Stop();
+            //Returns the result
             return returner;
         }
+
+        //Starts an instance and connection
+        private static Tuple<ISqlLocalDbInstanceInfo,ISqlLocalDbInstanceManager> InstanceStart() {
+            //Creates an object with info about the localdb installations
+            ISqlLocalDbApi localDB = new SqlLocalDbApi();
+            //Starts a localdb instance with name "Star-shl" with localdb version 15 (download provided on the drive, TODO: Add this to system requirements)
+            var instance = localDB.CreateInstance("Star-shl", "15.0");
+            //Allows managing of the instance
+            ISqlLocalDbInstanceManager manager = instance.Manage();
+            if (!instance.IsRunning)
+            {
+                manager.Start();
+            }
+            return new(instance,manager);
+        }
+        //Creates a connection
+        private static SqlConnection CreateConnection(Tuple<ISqlLocalDbInstanceInfo, ISqlLocalDbInstanceManager> info) => new((info.Item1.GetConnectionString() + $";AttachDbFilename={Directory.GetCurrentDirectory() }\\App_Data\\Database1.mdf;Integrated Security=True"));
+        //Example query, can be re-used for any query with returns a List<SqlTest>, hence there being 2 methods
+        private static List<Sqltest> P_ExampleQuery(string query)
+        {
+            //Starts the instance and connection
+            var info = InstanceStart();
+            var connection = CreateConnection(info);
+            connection.Open();
+            SqlCommand command = new(query, connection);
+            //Executes the query, output is a stream we need to turn into the requested output though
+            using SqlDataReader reader = command.ExecuteReader();
+            //Creates the list we will return
+            List<Sqltest> returner = new();
+            //Reads thourgh the stream
+            while (reader.Read())
+            {
+                //Gets the output per row
+                Sqltest value = new(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4), reader.GetString(5), reader.GetString(6));
+                //Use this as break point, value should have the requested data
+                returner.Add(value);
+            }
+            //Stops the connection and instance
+            connection.Close();
+            info.Item2.Stop();
+            //Returns the value
+            return returner;
+        }
+
         //Excecuted query, for clarity I expect all Sql statements in this class
         public static List<Sqltest> ExampleQuery() => P_ExampleQuery("select * from locations");
     }
