61d4611e85b442667c31f0fbf7766210bd7067ff
0992392 <80328928+NuurSaid@users.noreply.github.com>
2021-12-20 20:25:04 +0100

    Better inputs and error messages

--- VueJSDotnet51/ClientApp/src/components/AddReservation.vue
+++ VueJSDotnet51/ClientApp/src/components/AddReservation.vue
@@ -1,68 +1,68 @@
-<template>
-
+﻿<template>
     <h2>Uw gegevens</h2>
 
+    <form>
+        <label class="field field_v3">
+            <input class="field__input" v-model="form.firstName" name="firstName" id="firstName" type="text" placeholder="e.g. Anna" />
+            <span class="field__label-wrap">
+                <span class="field__label">Voorletters</span>
+                <p class="error">{{ msg['firstName'] }}</p>
+            </span>
+        </label>
 
-    <form class="form">
+        <label class="field field_v3">
+            <input class="field__input" v-model="form.insertion" placeholder="e.g. van den">
+            <span class="field__label-wrap">
+                <span class="field__label">Tussenvoegsel</span>
+            </span>
+        </label>
 
-        <div>
-            <label for="firstName">Voorletters:</label>
-            <input v-model="form.firstName" name="firstName" id="firstName" type="text" placeholder="Voorletters*" />
-            <p class="error">{{ msg['firstName'] }}</p>
-        </div>
+        <label class="field field_v3">
+            <input class="field__input" v-model="form.lastName" name="lastName" id="lastName" type="text" placeholder="e.g. Anna" />
+            <span class="field__label-wrap">
+                <span class="field__label">Achternaam</span>
+                <p class="error">{{ msg['lastName'] }}</p>
+            </span>
+        </label>
 
-        <div>
-            <label for="insertion">Tussenvoegsel:</label>
-            <input class="insertion_input" v-model="form.insertion" placeholder="Tussenvoegsel" />
-        </div>
+        <label class="field field_v3">
+            <input class="field__input" v-model="form.email" name="email" id="email" type="email" placeholder="e.g. melnik909@hotmail.nl">
+            <span class="field__label-wrap">
+                <span class="field__label">E-mail</span>
+                <p class="error">{{ msg['email'] }}</p>
+            </span>
+        </label>
 
-        <div>
-            <label for="lastName">Achternaam:</label>
-            <input v-model="form.lastName" name="lastName" id="lastName" type="text" placeholder="Achternaam*" />
-            <p class="error">{{ msg['lastName'] }}</p>
-        </div>
+        <label class="field field_v3">
+            <input class="field__input" v-model="form.phonenumber" name="phonenumber" id="phonenumber" type="number" placeholder="e.g. 0612345678" />
+            <span class="field__label-wrap">
+                <span class="field__label">Mobiel telefoonnummer</span>
+            </span>
+        </label>
 
-        <div>
-            <label for="email">E-mail:</label>
-            <input v-model="form.email" name="email" id="email" type="email" placeholder="E-mail*" />
-            <p class="error">{{ msg['email'] }}</p>
-        </div>
+        <label class="field field_v3">
+            <input class="field__input" v-model="form.age" type="number" name="age" id="age" min="0" max="130" />
+            <span class="field__label-wrap">
+                <span class="field__label">Leeftijd</span>
+                <p class="error">{{ msg['age'] }}</p>
+            </span>
+        </label>
 
-        <div>
-            <label for="phonenumber">Mobiel:</label>
-            <input v-model="form.phonenumber" name="phonenumber" id="phonenumber" type="number" placeholder="Mobiel telefoonnummer" />
-            <p class="error"></p>
-        </div>
 
-        <div>
-            <label for="age">Leeftijd:</label>
-            <input id="age" v-model="form.age" type="number" name="age" min="0" max="130">
-            <p class="error">{{ msg['age'] }}</p>
-        </div>
-
-        <div>
-            <label for="gender">Geslacht:</label>
-            <select name="gender" v-model="form.gender">
+        <label class="field field_v1">
+            <select class="field__input" name="gender" v-model="form.gender">
                 <option value="man">man</option>
                 <option value="woman">vrouw</option>
             </select>
-            <p class="error">{{ msg['gender'] }}</p>
-        </div>
-
-         <div class="btn" v-on:click="formCheck">Submit</div>
+            <span class="field__label-wrap">
+                <span class="field__label">Geslacht</span>
+                <p class="error">{{ msg['gender'] }}</p>
+            </span>
+        </label>
     </form>
 
-    <footer>
-        <p class="text-white">
-            <strong>Etten-Leur</strong><br>
-            Bredaseweg 165, 4872 LA<br>
-            Postbus 228, 4870 AE<br>
-            <br>
-            <strong>Rotterdam</strong><br>
-            Vlambloem 21, 3068 JE<br>
-            Postbus 8661, 3009 AR<br>
-        </p>
-    </footer>
+    <div class="btn" style="background-color: #4169E1; margin-top: 5%;" v-on:click="formCheck">Submit</div>
+
 </template>
 
 <script>
@@ -159,29 +159,201 @@
 
 
 <style scoped>
-    .form div {
-        margin: 10%;
-        text-align: left;
-    }
-
-    .form div label {
-        margin-right: 2%;
-    }
-
-    .insertion_input {
-        width: 50%;
-    }
-
-    .h2 {
-        margin-bottom: 15%;
+    .error {
+        color: #e7334c;
+        font-size: 80%;
     }
 
     footer {
         margin-top: 10vh;
     }
 
-    .error {
-        color: #e7334c;
+    .field__input {
+        --uiFieldPlaceholderColor: var(--fieldPlaceholderColor, #767676);
+        background-color: transparent;
+        border-radius: 0;
+        border: none;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        font-family: inherit;
+        font-size: inherit;
+    }
+
+        .field__input:focus::-webkit-input-placeholder {
+            color: var(--uiFieldPlaceholderColor);
+        }
+
+        .field__input:focus::-moz-placeholder {
+            color: var(--uiFieldPlaceholderColor);
+        }
+
+    /*
+=====
+CORE STYLES
+=====
+*/
+
+    .field {
+        --uiFieldBorderWidth: var(--fieldBorderWidth, 2px);
+        --uiFieldPaddingRight: var(--fieldPaddingRight, 1rem);
+        --uiFieldPaddingLeft: var(--fieldPaddingLeft, 1rem);
+        --uiFieldBorderColorActive: var(--fieldBorderColorActive, rgba(22, 22, 22, 1));
+        display: var(--fieldDisplay, inline-flex);
+        position: relative;
+        font-size: var(--fieldFontSize, 1rem);
+    }
+
+    .field__input {
+        box-sizing: border-box;
+        width: var(--fieldWidth, 100%);
+        height: var(--fieldHeight, 3rem);
+        padding: var(--fieldPaddingTop, 1.25rem) var(--uiFieldPaddingRight) var(--fieldPaddingBottom, .5rem) var(--uiFieldPaddingLeft);
+        border-bottom: var(--uiFieldBorderWidth) solid var(--fieldBorderColor, rgba(0, 0, 0, .25));
+    }
+
+        .field__input:focus {
+            outline: none;
+        }
+
+        .field__input::-webkit-input-placeholder {
+            opacity: 0;
+            transition: opacity .2s ease-out;
+        }
+
+        .field__input::-moz-placeholder {
+            opacity: 0;
+            transition: opacity .2s ease-out;
+        }
+
+        .field__input:focus::-webkit-input-placeholder {
+            opacity: 1;
+            transition-delay: .2s;
+        }
+
+        .field__input:focus::-moz-placeholder {
+            opacity: 1;
+            transition-delay: .2s;
+        }
+
+    .field__label-wrap {
+        box-sizing: border-box;
+        pointer-events: none;
+        cursor: text;
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+    }
+
+        .field__label-wrap::after {
+            content: "";
+            box-sizing: border-box;
+            width: 100%;
+            height: 0;
+            opacity: 0;
+            position: absolute;
+            bottom: 0;
+            left: 0;
+        }
+
+    .field__input:focus ~ .field__label-wrap::after {
+        opacity: 1;
+    }
+
+    .field__label {
+        position: absolute;
+        left: var(--uiFieldPaddingLeft);
+        top: calc(50% - .5em);
+        line-height: 1;
+        font-size: var(--fieldHintFontSize, inherit);
+        transition: top .2s cubic-bezier(0.9, -0.15, 0.1, 1.15), opacity .2s ease-out, font-size .2s ease-out;
+        will-change: bottom, opacity, font-size;
+    }
+
+    .field__input:focus ~ .field__label-wrap .field__label,
+    .field__input:not(placeholder-shown) ~ .field__label-wrap .field__label {
+        --fieldHintFontSize: var(--fieldHintFontSizeFocused, .75rem);
+        top: var(--fieldHintTopHover, .25rem);
+    }
+
+    /* 
+effect 1
+*/
+
+    .field_v1 .field__label-wrap::after {
+        border-bottom: var(--uiFieldBorderWidth) solid var(--uiFieldBorderColorActive);
+        transition: opacity .2s ease-out;
+        will-change: opacity;
+    }
+
+    /* 
+effect 2
+*/
+
+    .field_v2 .field__label-wrap {
+        overflow: hidden;
+    }
+
+        .field_v2 .field__label-wrap::after {
+            border-bottom: var(--uiFieldBorderWidth) solid var(--uiFieldBorderColorActive);
+            transform: translate3d(-105%, 0, 0);
+            will-change: transform, opacity;
+            transition: transform .285s ease-out .2s, opacity .2s ease-out .2s;
+        }
+
+    .field_v2 .field__input:focus ~ .field__label-wrap::after {
+        transform: translate3d(0, 0, 0);
+        transition-delay: 0;
+    }
+
+    /*
+effect 3
+*/
+
+    .field_v3 .field__label-wrap::after {
+        border: var(--uiFieldBorderWidth) solid var(--uiFieldBorderColorActive);
+        will-change: opacity, height;
+        transition: height .2s ease-out, opacity .2s ease-out;
+    }
+
+    .field_v3 .field__input:focus ~ .field__label-wrap::after {
+        height: 100%;
+    }
+
+    /*
+=====
+LEVEL 4. SETTINGS
+=====
+*/
+
+    .field {
+        --fieldBorderColor: #4169E1;
+        --fieldBorderColorActive: #191970;
+    }
+
+    /*
+=====
+DEMO
+=====
+*/
+
+    body {
+        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Open Sans, Ubuntu, Fira Sans, Helvetica Neue, sans-serif;
+        margin: 0;
+        min-height: 100vh;
+        display: flex;
+        flex-direction: column;
+    }
+
+    form {
+        box-sizing: border-box;
+        width: 100%;
+        max-width: 480px;
+        margin: 5% auto 0 auto;
+        padding: 1rem;
+        display: grid;
+        grid-gap: 30px;
     }
 </style>
 
